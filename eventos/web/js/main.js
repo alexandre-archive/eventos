0b03624d4e34436db7c9be8d1028f57cf205dc59
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2014-06-27 13:23:21 -0300

    Mais fru fru.

--- eventos/web/js/main.js
+++ eventos/web/js/main.js
@@ -1,1 +1,14 @@
+var App = angular.module('App', [])
+        .filter('SimNao', function() {
+            return function(input) {
+                return input ? "Sim" : "Não";
+            };
+        })
+        .run(function($rootScope, $http) {
+            $http.defaults.headers.common['x-ng-request'] = true;
 
+            $rootScope.User = null;
+        });
+
+App.controller('LoginCtrl', ['$scope', '$http', '$sce', '$rootScope', '$q', function($scope, $http, $sce, $rootScope, $q) {
+    }]);
