fcf6cd0028242b34963e8004bd38541be7408bc1
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2014-06-27 21:43:44 -0300

    Telas de signin, signup.

--- eventos/web/js/main.js
+++ eventos/web/js/main.js
@@ -7,8 +7,49 @@ var App = angular.module('App', [])
         .run(function($rootScope, $http) {
             $http.defaults.headers.common['x-ng-request'] = true;
 
-            $rootScope.User = null;
+            $rootScope.user = null;
+            
+            $rootScope.doLogout = function () {
+                $rootScope.user = null;
+                window.location.assign('/eventos/signin.html');
+            };
         });
 
 App.controller('LoginCtrl', ['$scope', '$http', '$sce', '$rootScope', '$q', function($scope, $http, $sce, $rootScope, $q) {
+
+        $scope.invalidLogin = false;
+        $scope.user = null;
+        $scope.pwd = null;
+
+        $scope.doLogin = function() {
+            if ($scope.user && $scope.pwd) {
+
+                $rootScope.user = {
+                    login: user,
+                };
+
+                window.location.assign("/eventos");
+            } else {
+                $scope.invalidLogin = true;
+            }
+        };
+
+        $scope.createAccount = function() {
+            window.location.assign('/eventos/signup.html');
+        };
+    }]);
+
+App.controller('JoinCtrl', ['$scope', '$http', '$sce', '$rootScope', '$q', function($scope, $http, $sce, $rootScope, $q) {
+
+        $scope.Dto = {
+            Name: "",
+            SurName: "",
+            Email: "",
+            Pwd: "",
+            PwdConfirmation: "",
+        };
+
+        $scope.joinSubmit = function() {
+            window.location.assign('/eventos');
+        };
     }]);
