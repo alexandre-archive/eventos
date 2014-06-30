64c46eef97899a4568e7bdbef6bf7595a4ef0538
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2014-06-30 09:29:10 -0300

    Remove reflection.

--- eventos/src/java/br/furb/eventos/ws/ApplicationConfig.java
+++ eventos/src/java/br/furb/eventos/ws/ApplicationConfig.java
@@ -3,10 +3,11 @@ package br.furb.eventos.ws;
 import java.util.Set;
 import java.util.logging.Logger;
 import javax.ws.rs.core.Application;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 
 /**
  * Classe de configuração dos Web Services.
- * 
+ *
  * @author Alexandre
  *
  * URL padrão: http://localhost:8084/eventos/api/
@@ -19,13 +20,18 @@ public class ApplicationConfig extends Application {
         Set<Class<?>> resources = new java.util.HashSet<>();
         addRestResourceClasses(resources);
 
-        try {
-            // Habilita suporte a JSON no Jersey.
-            Class jacksonProvider = Class.forName("org.codehaus.jackson.jaxrs.JacksonJsonProvider");
-            resources.add(jacksonProvider);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(getClass().getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+        /* TODO: Remover depois
+         try {
+         // Habilita suporte a JSON no Jersey.
+         Class jacksonProvider = Class.forName("org.codehaus.jackson.jaxrs.JacksonJsonProvider");
+         resources.add(jacksonProvider);
+         } catch (ClassNotFoundException ex) {
+         Logger.getLogger(getClass().getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         }
+         */
+        
+        // Habilita suporte a JSON no Jersey.
+        resources.add(JacksonJsonProvider.class);
 
         return resources;
     }
