7adaa8c0b9c171e6b7ba01c5f3b46891a4263535
unknown <priscila.kornely92@gmail.com>
2014-06-30 23:32:48 -0300

    gravar evento

--- eventos/src/java/br/furb/eventos/dto/NewEventDto.java
+++ eventos/src/java/br/furb/eventos/dto/NewEventDto.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.furb.eventos.dto;
 
-/**
- *
- * @author Alexandre
- */
 public class NewEventDto {
 
+    private long id;
     private String title;
     private String initialdate;
     private String finaldate;
